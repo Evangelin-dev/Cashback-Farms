49da281e695b6ccc59ab124199607f786db4ebbf
cseazeem <azeema224143@gmail.com>
2025-06-30 15:59:20 +0530

    Implemented user feature and kyc sectction

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,7 +7,11 @@ from .views import (
     RealEstateAgentProfileViewSet, RealEstateAgentRegistrationView,
     UserRegistrationView, OTPRequestView, OTPVerificationAndLoginView,
     UserLoginView, UserLogoutView, ReferralNetworkView,
-    SQLFTProjectViewSet, PlotInquiryViewSet, BankDetailViewSet, UserRegisterView
+    SQLFTProjectViewSet, PlotInquiryViewSet, BankDetailViewSet, UserRegisterView, UserProfileView,
+    KYCSubmitView, KYCStatusView, KYCUpdateView, MicroPlotListView, MicroPlotDetailView, FAQViewSet,
+    MaterialProductViewSet, SupportTicketViewSet, PlotPurchaseListView, PlotPurchaseCreateView, MicroPlotPurchaseListView, MicroPlotPurchaseCreateView,
+    MaterialPurchaseListView, MaterialPurchaseCreateView, ServiceOrderListView, ServiceOrderCreateView,SubmitPlotInquiry, SubmitMicroPlotInquiry, 
+    SubmitMaterialInquiry, SubmitServiceInquiry
 )
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -26,6 +30,17 @@ router.register(r'agents', RealEstateAgentProfileViewSet)
 router.register(r'sqlft-projects', SQLFTProjectViewSet)
 router.register(r'plot-inquiries', PlotInquiryViewSet)
 router.register(r'bank-details', BankDetailViewSet)
+router.register(r'materials', MaterialProductViewSet, basename='materials')
+router.register(r'ecommerce/materials', MaterialProductViewSet, basename='ecommerce-materials')
+router.register(r'ecommerce/services', EcommerceProductViewSet, basename='ecommerce-services')
+router.register(r'faqs', FAQViewSet, basename='faq')
+
+
+support_ticket = SupportTicketViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+})
+
 
 urlpatterns = [
     path('auth/register/', UserRegistrationView.as_view(), name='register'),
@@ -38,5 +53,27 @@ urlpatterns = [
     path('auth/jwt/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('referral/network/', ReferralNetworkView.as_view(), name='referral-network'),
-    path('auth/user-register/', UserRegisterView.as_view(), name='user-register')
+    path('auth/user-register/', UserRegisterView.as_view(), name='user-register'),
+    path('user/profile/', UserProfileView.as_view(), name='user-profile'),
+    path('user/kyc/submit/', KYCSubmitView.as_view(), name='kyc-submit'),
+    path('user/kyc/status/', KYCStatusView.as_view(), name='kyc-status'),
+    path('user/kyc/update/', KYCUpdateView.as_view(), name='kyc-update'),
+    path('micro-plots/', MicroPlotListView.as_view(), name='micro-plot-list'),
+    path('micro-plots/<int:pk>/', MicroPlotDetailView.as_view(), name='micro-plot-detail'),
+    path('api/', include(router.urls)),
+    path('support/inquiry/', SupportTicketViewSet.as_view({'post': 'submit_ticket'}), name='support-inquiry'),
+    path('support/my-tickets/', SupportTicketViewSet.as_view({'get': 'my_tickets'}), name='support-my-tickets'),
+    path('support/ticket/<int:pk>/', support_ticket, name='support-ticket-detail'),
+    path('purchase/plots/', PlotPurchaseListView.as_view(), name='purchase-plot-list'),
+    path('purchase/plot/', PlotPurchaseCreateView.as_view(), name='purchase-plot-create'),
+    path('purchase/micro-plots/', MicroPlotPurchaseListView.as_view(), name='micro-plot-purchase-list'),
+    path('purchase/micro-plot/', MicroPlotPurchaseCreateView.as_view(), name='micro-plot-purchase'),
+    path('purchase/materials/', MaterialPurchaseListView.as_view(), name='material-purchase-list'),
+    path('purchase/material/', MaterialPurchaseCreateView.as_view(), name='material-purchase'),
+     path('purchase/services/', ServiceOrderListView.as_view(), name='purchase-services'),
+    path('purchase/service/', ServiceOrderCreateView.as_view(), name='purchase-service'),
+    path('inquiry/plot/', SubmitPlotInquiry.as_view(), name='inquiry-plot'),
+    path('inquiry/micro-plot/', SubmitMicroPlotInquiry.as_view(), name='inquiry-micro-plot'),
+    path('inquiry/material/', SubmitMaterialInquiry.as_view(), name='inquiry-material'),
+    path('inquiry/service/', SubmitServiceInquiry.as_view(), name='inquiry-service'),    
 ]
