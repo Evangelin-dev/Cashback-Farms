bd787414b5acb34b6b0c15f14b662f7fd8f5be5f
lokeshnarayanasamy25@gmail.com <lokeshnarayanasamy25@gmail.com>
2025-07-31 20:51:12 +0530

    Office email smtp updated

--- backend/greenheap/settings.py
+++ backend/greenheap/settings.py
@@ -165,12 +165,13 @@ INSTALLED_APPS += [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "support@cashbackfarms.com"         # Replace with your Gmail address
-EMAIL_HOST_PASSWORD = "gfpfqmgdwwggbtyz"        # Replace with your Gmail App Password
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = 'support@cashbackfarms.com'
+EMAIL_HOST_PASSWORD = 'mrwsngrnlckjrwgf'
+DEFAULT_FROM_EMAIL = 'support@cashbackfarms.com'
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite default
